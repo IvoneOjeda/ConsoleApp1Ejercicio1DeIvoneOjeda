60278a754886429d1dd4375701fabba6bfca3835
Ivone Ojeda <Ivone Ojeda@CIC03FD516D4A2>
2022-07-13 12:50:32 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1Ejercicio1DeIvoneOjeda/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1Ejercicio1DeIvoneOjeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double euros;
+            double dolar;
+            double dolareuro;
+            double mercadoparalelo;
+            double pesos;
+
+
+            Console.Write("Ingresar cantidad de Dolares: ");
+            dolar = double.Parse(Console.ReadLine());
+
+
+            pesos = dolar * 230;
+            mercadoparalelo = dolar * 280;
+            dolareuro = dolar / 1.17;
+
+            Console.WriteLine($"Se puede comprar por {dolar} Dolar(es) {pesos} Pesos");
+            Console.WriteLine($"En el mercado pararlelo equivale a {mercadoparalelo} Pesos");
+            Console.WriteLine($"Y se pueden comprar {dolareuro} Euros");
+
+
+            Console.ReadLine();
+        }
+    }
+}
